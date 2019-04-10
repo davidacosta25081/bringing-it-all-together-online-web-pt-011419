4a42a32ccef9f749b72061ec97b6677f47daec20
David Acosta <davidacosta25081@yahoo.com>
2019-04-10 15:31:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -61,11 +61,11 @@ class Dog
 
 
   def self.new_from_db(row)
-    dog = Dog.new
-    dog.id = row[0]
-    dog.name = row[1]
-    dog.breed = row[2]
-    dog
+    
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id:id,name:name,breed:breed)
   end
 
 
